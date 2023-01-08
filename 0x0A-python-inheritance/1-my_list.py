3ebe17e0b147261959922773eee9eb4d11f697ab
magnes158 <mohamedabdiali158@gmail.com>
2023-01-08 02:33:42 -0800

    0x0A

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""My list
+Author: Kanneh
+"""
+
+
+class MyList(list):
+    """A class MyList that inherits from list"""
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry module.
+Author: Kanneh
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass
